2ec4977f6c75a237530725644a6f4faab1e2f182
Margenshteyn <m.margenshteyn@mail.ru>
2020-05-21 14:06:29 +0300

    Replaced in application.properties the org.hibernate.dialect.MySQL5InnoDBDialect  with a org.hibernate.dialect.MySQLInnoDBDialect and  com.mysql.jdbc.Driver  with  com.mysql.cj.jdbc.Driver Added spring-boot-starter-data-jpa in pom for annotation Entity  Added Entity User with fields  Added interfaces DAO and Service without methods

--- src/main/java/ru/javamentor/model/User.java
+++ src/main/java/ru/javamentor/model/User.java
@@ -1,4 +1,21 @@
 package ru.javamentor.model;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+    @Column(name = "firstName")
+    String firstName;
+    @Column(name = "lastName")
+    String lastName;
+    @Column(name = "userName")
+    String username;
+    @Column(name = "password")
+    String password;
+
+
 }
