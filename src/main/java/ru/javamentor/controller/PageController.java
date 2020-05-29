6ab7500a67cfa9daa9061ba907138cce6850823d
Sergey Unschikov <600522@mail.ru>
2020-05-29 15:45:45 +0300

    v.1

--- src/main/java/ru/javamentor/controller/PageController.java
+++ src/main/java/ru/javamentor/controller/PageController.java
@@ -1,14 +1,24 @@
 package ru.javamentor.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import ru.javamentor.model.Topic;
+import ru.javamentor.service.TopicService;
 
 
 //@org.springframework.stereotype.Controller
 @Controller
 public class PageController {
+
+    public final TopicService topicService;
+
+    public PageController(TopicService topicService) {
+        this.topicService = topicService;
+    }
 
 
     @RequestMapping(value = "/*", method = RequestMethod.GET)
@@ -24,6 +34,13 @@ public class PageController {
     @GetMapping("/index")
     public String indexPage() {
         return "index";
+    }
+
+    @GetMapping("/topic/id")
+    public String topicPage(@PathVariable Long id, Model model) {
+        Topic topic = topicService.getTopicById(id);
+        model.addAttribute("topic", topic);
+        return "topic";
     }
 
 }
