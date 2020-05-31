75843a7e660cfe82d8eb5d242fe6e7a3c98fa543
Sergey Unschikov <600522@mail.ru>
2020-05-31 21:19:51 +0300

    создана папка rest. переименованы рест контроллеры

--- src/main/java/ru/javamentor/controller/rest/UserRestControllers.java
+++ src/main/java/ru/javamentor/controller/rest/UserRestControllers.java
@@ -1,4 +1,4 @@
-package ru.javamentor.controller;
+package ru.javamentor.controller.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,14 +15,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-public class UserControllers {
+public class UserRestControllers {
 
     private UserService userService;
 
     private RoleService roleService;
 
     @Autowired
-    public UserControllers(UserService userService, RoleService roleService)
+    public UserRestControllers(UserService userService, RoleService roleService)
     {
         this.userService = userService;
         this.roleService = roleService;
