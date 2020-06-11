5e992ab60f63b147e9ae93fc1d1453bfde5767d9
Gessal <gessalmaxim@gmail.com>
2020-06-11 12:18:37 +0300

    Админка - добавил навигацию по страницам списка не модерированных топиков

--- src/main/resources/static/js/admin-moderate-events.js
+++ src/main/resources/static/js/admin-moderate-events.js
@@ -1,25 +1,37 @@
 $(document).ready(function(){
-    $('#admin_miderate_link').addClass('active');
-    getAndPrintNotModeratedTopicsPage(1, $('#moderate_cards'));
+    let admin_moderate_link = $('#admin_moderate_link');
+    let moderate_cards = $('#moderate_cards');
+    let page_number = 1;
+
+    admin_moderate_link.addClass('active');
+    getAndPrintNotModeratedTopicsPage(1, moderate_cards);
     //размер страницы жестко задан
     let pageSize = 5;
     getNotModeratedTopicsCount().then(count => {
         let page_buttons = $('#page_buttons');
         page_buttons.empty();
-        page_buttons.append('<li class="page-item disabled">' +
-                                '<button class="page-link" type="button" tabindex="-1" aria-disabled="true">Previous</button>' +
-                            '</li>' +
-                            '<li class="page-item active">' +
-                                '<button class="page-link" type="button">1</button>' +
+        page_buttons.append('<li id="pi_1" class="page-item active">' +
+                                '<button id="p_1" class="page-link page-btn" type="button">1</button>' +
                             '</li>');
 
         for (let i = 2; i <= Math.ceil(count/pageSize); i++) {
-            page_buttons.append('<li class="page-item">' +
-                                    '<button class="page-link" type="button">' + i + '</button>' +
+            page_buttons.append('<li id="pi_' + i + '" class="page-item">' +
+                                    '<button id="p_' + i + '" class="page-link page-btn" type="button">' + i + '</button>' +
                                 '</li>');
         }
-        page_buttons.append('<li class="page-item">' +
-                                '<button class="page-link" type="button">Next</button>' +
-                            '</li>');
+    });
+    /**
+     * обработчик нажатия на кнопки навигации по страницам
+     */
+    $(document).on('click', '.page-btn', function() {
+        let new_page_number = this.id.replace("p_", '');
+        if (new_page_number > page_number || new_page_number < page_number) {
+            moderate_cards.empty();
+            getAndPrintNotModeratedTopicsPage(new_page_number, moderate_cards);
+            $('#pi_' + page_number).removeClass('active');
+            $('#pi_' + new_page_number).addClass('active');
+            page_number = new_page_number;
+
+        }
     });
 });
