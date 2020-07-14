cb751cba8db99cbc41cca57717a6e64217e07161
Elmaximka <60235940+Elmaximka@users.noreply.github.com>
2020-07-14 21:57:18 +0500

    1. Open Modal window if User is UnAuthenticated

--- src/main/resources/static/js/addComment.js
+++ src/main/resources/static/js/addComment.js
@@ -1,4 +1,7 @@
 document.getElementById('add_comment_button').onclick = function () {
+    //Открывает модальное окно для неавторизованных пользователей
+    $('#authorizationModal').modal('show');
+
     let commentBody = document.getElementById('textareaResize');
     let comment = commentBody.value.replace(/\n/g, '<br />');
     let path = location.pathname.split('/');
@@ -13,17 +16,18 @@ document.getElementById('add_comment_button').onclick = function () {
     document.getElementById('textareaResize').style.height = "auto";
     $('#counter').html(2000);
 
-    return fetch('/api/user/comment/add', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(data)
+        return fetch('/api/user/comment/add', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(data)
 
-    })
-        .then(result => result.json())
-        .then(comment => {
-            let commentCard = commentInCard(comment);
-            $('#comments_container').prepend(commentCard);
-        });
+        })
+            .then(result => result.json())
+            .then(comment => {
+                let commentCard = commentInCard(comment);
+                $('#comments_container').prepend(commentCard);
+            });
+
 }
