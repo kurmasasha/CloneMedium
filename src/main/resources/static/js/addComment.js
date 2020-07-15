25dbfbced29fb18a1e65ca6dd5512e97baf78274
murrengan <180107250@stu.sdu.edu.kz>
2020-07-16 03:53:16 +0600

    не будет отправляться пустой комментарий

--- src/main/resources/static/js/addComment.js
+++ src/main/resources/static/js/addComment.js
@@ -8,26 +8,28 @@ document.getElementById('add_comment_button').onclick = function () {
     let path = location.pathname.split('/');
     let topicId = path[path.length - 1];
 
-    let data = {
-        comment: comment,
-        topicId: topicId
+    if(comment !== '') {
+        let data = {
+            comment: comment,
+            topicId: topicId
+        }
+
+        document.getElementById('textareaResize').value = '';
+        document.getElementById('textareaResize').style.height = "auto";
+        $('#counter').html(2000);
+
+        return fetch('/api/user/comment/add', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(data)
+
+        })
+            .then(result => result.json())
+            .then(comment => {
+                let commentCard = commentInCard(comment);
+                $('#comments_container').prepend(commentCard);
+            });
     }
-
-    document.getElementById('textareaResize').value = '';
-    document.getElementById('textareaResize').style.height = "auto";
-    $('#counter').html(2000);
-
-    return fetch('/api/user/comment/add', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(data)
-
-    })
-        .then(result => result.json())
-        .then(comment => {
-            let commentCard = commentInCard(comment);
-            $('#comments_container').prepend(commentCard);
-        });
 }
