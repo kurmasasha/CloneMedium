6a3e5f82437e4229547a4c8993919801313c5f9d
Dima Ian <dima.ian.80@gmail.com>
2020-07-22 18:58:15 +0300

    NotificationRestController's cleaned.

--- src/main/java/ru/javamentor/controller/rest/NotificationRestController.java
+++ src/main/java/ru/javamentor/controller/rest/NotificationRestController.java
@@ -88,39 +88,4 @@ public class NotificationRestController {
     }
 
 
-
-//    private static int counter = 0;
-//    private static int randomNbr = 0;
-
-    /**
-     *  Возвращает количество новых уведомлений для залогиненого юзера - "ЗАГЛУШКА - имитатор"
-     * @param 'Principal User'
-     * @return случайное число нотификаций
-     */
-//    @GetMapping("/user/MyNotifsNbr")
-//    public ResponseEntity<Integer> getNumberOfNotificationsNbrDumper(@AuthenticationPrincipal User user) throws InterruptedException {
-//        switch (counter) {
-//            case 0:
-//                counter++;
-//                randomNbr = ThreadLocalRandom.current().nextInt(1, 12 + 1);
-//                Thread.sleep(5000);
-//                return new ResponseEntity<>( randomNbr, HttpStatus.OK);
-//            case  1:
-//                counter++;
-//                return new ResponseEntity<>( randomNbr, HttpStatus.OK);
-//            case 2:
-//                counter++;
-//                return new ResponseEntity<>( randomNbr, HttpStatus.OK);
-//            case 3:
-//                counter++;
-//                return new ResponseEntity<>( randomNbr, HttpStatus.OK);
-//            case 4:
-//                counter++;
-//                return new ResponseEntity<>( randomNbr, HttpStatus.OK);
-//            default:
-//                counter = 0;
-//                randomNbr = ThreadLocalRandom.current().nextInt(1, 12 + 1);
-//                return new ResponseEntity<>( randomNbr, HttpStatus.OK);
-//        }
-//    }
 }
