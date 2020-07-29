62edfd5f75b511ab959e70160b18dae16446ab1c
Elmaximka <60235940+Elmaximka@users.noreply.github.com>
2020-07-29 21:02:26 +0500

    Refactor Social Configs Refactor RestController

---
+++ src/main/java/ru/javamentor/config/SocialConfig.java
@@ -0,0 +1,31 @@
+package ru.javamentor.config;
+
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import ru.javamentor.model.User;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public interface SocialConfig {
+
+    /*
+        Метод для сборки URL из clientId, clientSecret и callbackUrl приложения разработчика, которые отправляются в RestController
+        Поле OAuth20Service инициализируется один раз при первой авторизации
+     */
+    String getAuthorizationUrl();
+
+    /**
+     * Метод для получения OAuth2AccessToken
+     *
+     * @param code -параметр , с которым возвращается пользователь
+     * @return OAuth2AccessToken
+     */
+    OAuth2AccessToken toGetToken(String code) throws InterruptedException, ExecutionException, IOException;
+    /**
+     * Метод для создания нового пользователя с помошью OAuth2AccessToken
+     *
+     * @param token - токен
+     * @return User - пользователь в системе
+     */
+    User toCreateUser(OAuth2AccessToken token) throws InterruptedException, ExecutionException, IOException;
+}
