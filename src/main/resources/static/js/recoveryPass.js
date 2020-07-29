0d82055a53237f7807a62dca66e218d4b434bbad
TarasBulba007 <mr.arra@yandex.ru>
2020-07-29 20:11:47 +0300

    merge conflicts at login.html

---
+++ src/main/resources/static/js/recoveryPass.js
@@ -0,0 +1,26 @@
+$("#recoverPassword").on("click", async function () {
+    let mail = $("#email").val();
+    let message = document.getElementById('messageForUser');
+    message.innerHTML = "";
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/html;charset=UTF-8'
+        }
+    };
+    let URL = '/api/free-user/recoveryPassword/getCode/?email=' + mail;
+    await fetch(URL, options)
+        .then(
+            response => {
+                if (response.ok) {
+                    let form = document.getElementById('Forgot-Password-Form');
+                    form.innerHTML = "Вам на почту отправлено письмо с инструкцией для восстановления пароля"
+                } else {
+                    message.style.color = "red";
+                    message.innerHTML = "Пользователя с таким e-mail не существует!";
+                }
+            }
+        )
+});
+
+
