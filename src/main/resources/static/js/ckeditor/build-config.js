b181a3dfe10c1bdd9e9c063fd3d8916adc866eec
dmitr2ish <dmitr2ish@yandex.ru>
2020-08-28 15:28:57 +0500

    - added CKeditor custom build: skin: Moono Dark; plugins: Basic style https://ckeditor.com/cke4/addon/basicstyles, Blockquote https://ckeditor.com/cke4/addon/blockquote, ClipBoard https://ckeditor.com/cke4/addon/clipboard, Context Menu https://ckeditor.com/cke4/addon/contextmenu, Editor Resize https://ckeditor.com/cke4/addon/resize, Editor Toolbar https://ckeditor.com/cke4/addon/toolbar (three buttons was hidden), Elements Path https://ckeditor.com/cke4/addon/elementspath, Enter Key https://ckeditor.com/cke4/addon/enterkey, Escape HTML Entities  https://ckeditor.com/cke4/addon/entities, Floating Space https://ckeditor.com/cke4/addon/floatingspace, Format https://ckeditor.com/cke4/addon/format, Horizontal Rule https://ckeditor.com/cke4/addon/horizontalrule, HTML Output Writer https://ckeditor.com/cke4/addon/htmlwriter, IFrame Editing Area https://ckeditor.com/cke4/addon/wysiwygarea, Indent List https://ckeditor.com/cke4/addon/indentlist, Upload Image https://ckeditor.com/cke4/addon/uploadimage, Undo https://ckeditor.com/cke4/addon/undo, Tab Key Handling https://ckeditor.com/cke4/addon/tab, Styles Combo https://ckeditor.com/cke4/addon/stylescombo, SpellCheckAsYouType (SCAYT) https://ckeditor.com/cke4/addon/scayt, Special Characters https://ckeditor.com/cke4/addon/specialchar, Source Editing Area https://ckeditor.com/cke4/addon/sourcearea, Show Table Borders https://ckeditor.com/cke4/addon/showborders, Remove Format https://ckeditor.com/cke4/addon/removeformat, Paste Tools https://ckeditor.com/cke4/addon/pastetools, Paste from Word https://ckeditor.com/cke4/addon/pastefromword, Paste from Google Docs https://ckeditor.com/cke4/addon/pastefromgdocs, Paste As Plain Text https://ckeditor.com/cke4/addon/pastetext, Maximize https://ckeditor.com/cke4/addon/maximize, Magic Line https://ckeditor.com/cke4/addon/magicline, List https://ckeditor.com/cke4/addon/list

---
+++ src/main/resources/static/js/ckeditor/build-config.js
@@ -0,0 +1,91 @@
+﻿/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/license
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) https://ckeditor.com/cke4/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) https://ckeditor.com/cke4/builder/1a984f625e2a202b5a65b8c54fd902bc
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) https://ckeditor.com/cke4/builder/download/1a984f625e2a202b5a65b8c54fd902bc
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'moono-dark',
+	preset: 'standard',
+	ignore: [
+		'.DS_Store',
+		'.bender',
+		'.editorconfig',
+		'.gitattributes',
+		'.gitignore',
+		'.idea',
+		'.jscsrc',
+		'.jshintignore',
+		'.jshintrc',
+		'.mailmap',
+		'.npm',
+		'.travis.yml',
+		'bender-err.log',
+		'bender-out.log',
+		'bender.ci.js',
+		'bender.js',
+		'dev',
+		'gruntfile.js',
+		'less',
+		'node_modules',
+		'package.json',
+		'tests'
+	],
+	plugins : {
+		'basicstyles' : 1,
+		'blockquote' : 1,
+		'clipboard' : 1,
+		'contextmenu' : 1,
+		'elementspath' : 1,
+		'enterkey' : 1,
+		'entities' : 1,
+		'floatingspace' : 1,
+		'format' : 1,
+		'horizontalrule' : 1,
+		'htmlwriter' : 1,
+		'indentlist' : 1,
+		'list' : 1,
+		'magicline' : 1,
+		'maximize' : 1,
+		'pastefromgdocs' : 1,
+		'pastefromword' : 1,
+		'pastetext' : 1,
+		'pastetools' : 1,
+		'removeformat' : 1,
+		'resize' : 1,
+		'scayt' : 1,
+		'showborders' : 1,
+		'sourcearea' : 1,
+		'specialchar' : 1,
+		'stylescombo' : 1,
+		'tab' : 1,
+		'toolbar' : 1,
+		'undo' : 1,
+		'uploadimage' : 1,
+		'wysiwygarea' : 1
+	},
+	languages : {
+		'en' : 1,
+		'ru' : 1
+	}
+};
---
+++ src/main/resources/static/js/ckeditor/replaceTextArea.js
@@ -0,0 +1,2 @@
+CKEDITOR.replace( 'editTopicContent' );
+CKEDITOR.replace( 'topic_content' );
