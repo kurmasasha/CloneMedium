3aa32e32d14c1aec3a74bbbab4fa80e4f93104d1
Yuriy Sergeev <Gmadbull@gmail.com>
2020-09-05 12:45:02 +0300

    Creates controller for receiving best five topics

---
+++ src/main/java/ru/javamentor/controller/rest/BestFiveOfTopicsRestController.java
@@ -0,0 +1,34 @@
+package ru.javamentor.controller.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.javamentor.model.Topic;
+import ru.javamentor.service.topic.best.BestTopicsService;
+
+import java.util.List;
+
+/**
+ * REST контроллер для получения
+ * 5-ти лучших статей по лайкам
+ *
+ * @version 1.0
+ * @author Java Mentor
+ */
+@RestController
+@RequestMapping("/best/topics")
+public class BestFiveOfTopicsRestController {
+    private final BestTopicsService bestTopicsService;
+
+    @Autowired
+    public BestFiveOfTopicsRestController(BestTopicsService bestTopicsService) {
+        this.bestTopicsService = bestTopicsService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Topic>> getFiveTopicsByLikes(){
+        return null;
+    }
+}
