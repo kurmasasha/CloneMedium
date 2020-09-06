ae78124efce06072b96e5c6297a4a0b644abad3a
Yuriy Sergeev <Gmadbull@gmail.com>
2020-09-06 19:13:53 +0300

    Creates BestCommentRestController

---
+++ src/main/java/ru/javamentor/controller/rest/BestCommentRestController.java
@@ -0,0 +1,10 @@
+package ru.javamentor.controller.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("best/comment")
+public class BestCommentRestController {
+
+}
