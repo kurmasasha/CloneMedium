6458b7dbedffe09a91cc3846df7524145fee1d0e
Yuriy Sergeev <Gmadbull@gmail.com>
2020-09-07 19:28:55 +0300

    Adds transactional to service

--- src/main/java/ru/javamentor/service/topic/best/BestTopicsServiceImpl.java
+++ src/main/java/ru/javamentor/service/topic/best/BestTopicsServiceImpl.java
@@ -3,6 +3,7 @@ package ru.javamentor.service.topic.best;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javamentor.dao.topic.best.BestTopicsDAO;
 import ru.javamentor.model.Topic;
 
@@ -24,6 +25,7 @@ public class BestTopicsServiceImpl implements BestTopicsService {
         this.bestTopicsDAO = bestTopicsDAO;
     }
 
+    @Transactional
     @Override
     public List<Topic> bestFive() {
         return bestTopicsDAO.findAll(Sort.by(Sort.Direction.DESC, "likes"))
