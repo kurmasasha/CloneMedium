49e2b7238d22e37c18b6415db3c9073dc104269c
Yuriy Sergeev <Gmadbull@gmail.com>
2020-09-07 19:28:14 +0300

    Adds transactional to service

--- src/main/java/ru/javamentor/service/comment/best/BestCommentServiceImpl.java
+++ src/main/java/ru/javamentor/service/comment/best/BestCommentServiceImpl.java
@@ -3,6 +3,7 @@ package ru.javamentor.service.comment.best;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javamentor.dao.comment.best.BestCommentDAO;
 import ru.javamentor.model.Comment;
 
@@ -24,6 +25,7 @@ public class BestCommentServiceImpl implements BestCommentService {
         this.bestCommentDAO = bestCommentDAO;
     }
 
+    @Transactional
     @Override
     public List<Comment> bestFiveComment() {
         return bestCommentDAO.findAll(Sort.by(Sort.Direction.DESC, "likes"))
