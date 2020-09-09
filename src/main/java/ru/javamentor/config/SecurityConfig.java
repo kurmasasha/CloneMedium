8563c9f819dc50e382b4584c98d23385d8926e4b
Stepan Damrin <sr.stepan.damrin@gmail.com>
2020-09-09 13:01:56 +0300

    fixed trouble with starting jar

--- src/main/java/ru/javamentor/config/SecurityConfig.java
+++ src/main/java/ru/javamentor/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,76 +19,83 @@ import ru.javamentor.config.util.LoginSuccessHandler;
 /**
  * Класс отвечающий за конфигурацию секьюрности
  *
- * @version 1.0
  * @author Java Mentor
+ * @version 1.0
  */
 @Configuration
 @ComponentScan("ru.javamentor")
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private UserDetailsService userDetailsService;
+  private UserDetailsService userDetailsService;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+  @Autowired
+  public SecurityConfig(@Qualifier("userDetailServiceImpl") UserDetailsService userDetailsService) {
+    this.userDetailsService = userDetailsService;
+  }
 
-    @Autowired
-    public SecurityConfig(@Qualifier("userDetailServiceImpl") UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
+  /**
+   * метод устанавливает userDetailsService и passwordEncoder
+   *
+   * @param auth - объект с помощью которого конфигурируем аутентификацию
+   * @return void
+   */
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.authenticationProvider(authProvider());
+  }
 
-    /**
-     * метод устанавливает userDetailsService и passwordEncoder
-     *
-     * @param auth - объект с помощью которого конфигурируем аутентификацию
-     * @return void
-     */
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-    }
+  /**
+   * метод для установки секьюрности разным URL
+   *
+   * @param http - объект для конфигурации секьюрности
+   * @return void
+   */
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.formLogin()
 
-    /**
-     * метод для установки секьюрности разным URL
-     *
-     * @param http - объект для конфигурации секьюрности
-     * @return void
-     */
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
+        .loginPage("/login").failureUrl("/login?error=true")
+        .successHandler(new LoginSuccessHandler())
+        .loginProcessingUrl("/login")
+        .usernameParameter("username")
+        .passwordParameter("password")
+        .permitAll();
 
-                .loginPage("/login").failureUrl("/login?error=true")
-                .successHandler(new LoginSuccessHandler())
-                .loginProcessingUrl("/login")
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .permitAll();
+    http.logout()
+        .permitAll()
+        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+        .logoutSuccessUrl("/login?logout")
+        .and().csrf().disable();
+    http
+        .authorizeRequests()
+        .antMatchers("/", "/registration/**", "/activate/*", "/api/free-user/**",
+            "/js/**", "/webjars/**", "/css/**", "/img/**", "/api/topic/**", "/topic-img/**",
+            "/user-img/**")
+        .permitAll()
+        .antMatchers("/authorization/**", "/api/user/topic/**", "/topic/**", "/recoveryPass/**")
+        .permitAll()
+        .antMatchers("/login").anonymous()
+        .antMatchers("/admin/**", "/api/admin/**").hasAuthority("ADMIN")
+        .antMatchers("/api/user/**", "/user/**").hasAnyAuthority("USER", "ADMIN")
+        .anyRequest().authenticated();
+  }
 
-        http.logout()
-                .permitAll()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login?logout")
-                .and().csrf().disable();
-        http
-                .authorizeRequests()
-                .antMatchers("/", "/registration/**", "/activate/*", "/api/free-user/**",
-                                        "/js/**", "/webjars/**", "/css/**", "/img/**", "/api/topic/**", "/topic-img/**", "/user-img/**")
-                .permitAll()
-                .antMatchers("/authorization/**", "/api/user/topic/**", "/topic/**","/recoveryPass/**").permitAll()
-                .antMatchers("/login").anonymous()
-                .antMatchers("/admin/**", "/api/admin/**").hasAuthority("ADMIN")
-                .antMatchers("/api/user/**", "/user/**").hasAnyAuthority("USER", "ADMIN")
-                .anyRequest().authenticated();
-    }
+  /**
+   * метод конфигурирующий бин для шифрования пароля
+   *
+   * @return BCryptPasswordEncoder
+   */
+  @Bean
+  public PasswordEncoder getBCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-    /**
-     * метод конфигурирующий бин для шифрования пароля
-     *
-     * @return BCryptPasswordEncoder
-     */
-    @Bean
-    public PasswordEncoder getBCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  public DaoAuthenticationProvider authProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService);
+    authProvider.setPasswordEncoder(getBCryptPasswordEncoder());
+    return authProvider;
+  }
 }
