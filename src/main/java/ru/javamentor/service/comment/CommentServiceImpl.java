8680a10f3d762b81458c63394a66616d608efe9c
Yuriy Sergeev <Gmadbull@gmail.com>
2020-09-10 22:36:47 +0300

    Changes addComment method to CommentService
    Fixes problems with tests to CommentServiceImplTest

--- src/main/java/ru/javamentor/service/comment/CommentServiceImpl.java
+++ src/main/java/ru/javamentor/service/comment/CommentServiceImpl.java
@@ -12,6 +12,10 @@ import ru.javamentor.model.Comment;
 import ru.javamentor.model.Notification;
 import ru.javamentor.model.Topic;
 import ru.javamentor.model.User;
+import ru.javamentor.service.notification.NotificationService;
+import ru.javamentor.service.notification.WsNotificationService;
+import ru.javamentor.service.topic.TopicService;
+import ru.javamentor.service.user.UserService;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -29,11 +33,23 @@ public class CommentServiceImpl implements CommentService {
 
     private final UserDAO userDAO;
     private final CommentDAO commentDAO;
+    private final TopicService topicService;
+    private final UserService userService;
+    private final NotificationService notificationService;
+    private final WsNotificationService wsNotificationService;
 
     @Autowired
-    public CommentServiceImpl(CommentDAO commentDAO, UserDAO userDAO) {
+    public CommentServiceImpl(CommentDAO commentDAO,
+                              UserDAO userDAO,
+                              TopicService topicService,
+                              UserService userService,
+                              NotificationService notificationService, WsNotificationService wsNotificationService) {
         this.commentDAO = commentDAO;
         this.userDAO = userDAO;
+        this.topicService = topicService;
+        this.userService = userService;
+        this.notificationService = notificationService;
+        this.wsNotificationService = wsNotificationService;
     }
 
     /**
@@ -46,9 +62,12 @@ public class CommentServiceImpl implements CommentService {
     @Override
     public Comment addComment(CommentDTO commentDTO, User author) {
         try {
-            Comment comment = commentDTOToComment(commentDTO, author);
+//            checkEmptyComment(commentDTO);
 
+            Comment comment = commentDTOToComment(commentDTO, author);
             commentDAO.addComment(comment);
+
+            sendNotification(topicService.getTopicById(commentDTO.getTopicId()), author);
 
             log.debug("IN addComment - comment: {} with author.id: {} and author.userName: {} successfully added",
                     commentDTO.getText(), author.getId(), author.getUsername());
@@ -60,21 +79,31 @@ public class CommentServiceImpl implements CommentService {
         }
     }
 
-    private Comment commentDTOToComment(CommentDTO commentDTO, User author){
-        return null;
+    private void checkEmptyComment(CommentDTO commentDTO) throws Exception {
+        if (commentDTO.getText().isEmpty()) {
+            throw new Exception();
+        }
     }
 
-    private void sendNotification(){
+    private Comment commentDTOToComment(CommentDTO commentDTO, User author) {
+        return new Comment(commentDTO.getText(),
+                author,
+                topicService.getTopicById(commentDTO.getTopicId()),
+                LocalDateTime.now(), commentDTO.getIsMainComment(),
+                commentDTO.getMainCommentId());
+    }
+
+    private void sendNotification(Topic topic, User author) {
         for (User u : topic.getAuthors()) {
             Notification notification = new Notification();
 
             notification.setTitle("Новый комментарий");
-            notification.setText("Пользователь " +
-                    userService.getUserByEmail(auth.getName()) +
+            notification.setText("Пользователь " + userService.getUserByEmail(author.getUsername()) +
                     " оставил новый комментарий в статье " + topic.getTitle() + " ");
             notification.setUser(u);
 
             notificationService.addNotification(notification);
+
             wsNotificationService.sendNotification(u, notificationService.getNotificationDto
                     (notificationService.getById(notification.getId())));
         }
@@ -267,7 +296,8 @@ public class CommentServiceImpl implements CommentService {
 
     /**
      * Метод для првоерки на установление соответствия автора комментарию.
-     * @param userId -  id юзера
+     *
+     * @param userId    -  id юзера
      * @param commentId - id комментария
      */
     @Override
@@ -282,7 +312,7 @@ public class CommentServiceImpl implements CommentService {
 
     }
 
-    public boolean isExist(Long commentId){
+    public boolean isExist(Long commentId) {
         return commentDAO.isExist(commentId);
     }
 
