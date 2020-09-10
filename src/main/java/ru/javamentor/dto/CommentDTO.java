1fdefa9a1c44586500bfef330c9d37fff3e52110
Yuriy Sergeev <Gmadbull@gmail.com>
2020-09-10 15:33:30 +0300

    Adds new methods in CommentServiceImpl

--- src/main/java/ru/javamentor/dto/CommentDTO.java
+++ src/main/java/ru/javamentor/dto/CommentDTO.java
@@ -1,5 +1,12 @@
 package ru.javamentor.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class CommentDTO {
  private String text;
  private Long TopicId;
