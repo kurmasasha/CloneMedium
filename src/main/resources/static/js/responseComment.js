6dd4e088b09d64743cb8f3f73ae5725ca8af15e8
Yuriy Sergeev <Gmadbull@gmail.com>
2020-09-13 21:31:57 +0300

    Adds function commentTree to fillCommentContainer.js

--- src/main/resources/static/js/responseComment.js
+++ src/main/resources/static/js/responseComment.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).on('click', '.response-button', function () {
     $('#' + $(this).attr('data-panelId')).toggle();
 })
@@ -31,13 +30,5 @@ $(document).on('click', '#addReplyButton', async function () {
 
     $('#' + $(this).attr('data-panelId')).toggle();
 
-    await fillCommentFields();
-=======
-$('#replyButton').on('click', function () {
-    $('#' + $(this).attr('data-commentbox')).toggle();
-});
-
-$('#add_comment_answer').on('click', function () {
-    $('#' + $(this).attr('data-commentbox')).toggle();
->>>>>>> c7d23cd... Adds button and area for answer at comment
+    await fillCommentContainer();
 });
